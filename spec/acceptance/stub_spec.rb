17cdbc98d49ad39a21cc6fa80a22e3997b07657a
Scott Werner <paircast@Scotts-iMac.local>
2019-01-27 11:35:57 -0500

    Initial Commit

---
+++ spec/acceptance/stub_spec.rb
@@ -0,0 +1,60 @@
+describe "a stubbed instance" do
+  include FactoryBot::Syntax::Methods
+
+  before do
+    define_model("User")
+
+    define_model("Post", user_id: :integer) do
+      belongs_to :user
+    end
+
+    FactoryBot.define do
+      factory :user
+
+      factory :post do
+        user
+      end
+    end
+  end
+
+  subject { build_stubbed(:post) }
+
+  it "acts as if it came from the database" do
+    should_not be_new_record
+  end
+
+  it "assigns associations and acts as if it is saved" do
+    expect(subject.user).to be_kind_of(User)
+    expect(subject.user).not_to be_new_record
+  end
+end
+
+describe "a stubbed instance overriding strategy" do
+  include FactoryBot::Syntax::Methods
+
+  before do
+    define_model("User")
+    define_model("Post", user_id: :integer) do
+      belongs_to :user
+    end
+
+    FactoryBot.define do
+      factory :user
+
+      factory :post do
+        association(:user, strategy: :build)
+      end
+    end
+  end
+
+  subject { build_stubbed(:post) }
+
+  it "acts as if it is saved in the database" do
+    should_not be_new_record
+  end
+
+  it "assigns associations and acts as if it is saved" do
+    expect(subject.user).to be_kind_of(User)
+    expect(subject.user).not_to be_new_record
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,42 @@
+require "rspec"
+require "rspec/its"
+
+require "simplecov"
+
+require "factory_bot"
+require "timecop"
+
+Dir["spec/support/**/*.rb"].each { |f| require File.expand_path(f) }
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on a
+    # real object. This is generally recommended, and will default to `true` in
+    # RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  config.include DeclarationMatchers
+
+  config.before do
+    FactoryBot.reload
+  end
+
+  config.after do
+    Timecop.return
+  end
+
+  config.around do |example|
+    begin
+      previous_use_parent_strategy = FactoryBot.use_parent_strategy
+      example.run
+    ensure
+      FactoryBot.use_parent_strategy = previous_use_parent_strategy
+    end
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.example_status_persistence_file_path = "tmp/rspec_examples.txt"
+end
